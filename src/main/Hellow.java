5d397bc8743b35a931c2a77137540f72234e9fac
zyAlex <517434198@qq.com>
2019-05-22 10:08:27 +0800

    练习

---
+++ src/main/Hellow.java
@@ -0,0 +1,9 @@
+public class Hellow {
+    private String id = "";
+    private String name = "";
+
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println("");
+    }
+}
