5b365a9a89a13f792f30ed633bb67f2f727bbb58
zyAlex <517434198@qq.com>
2019-05-22 10:15:34 +0800

    完善

--- src/main/Hellow.java
+++ src/main/Hellow.java
@@ -1,6 +1,6 @@
 public class Hellow {
-    private String id = "";
-    private String name = "";
+    private String id = "1";
+    private String name = "zyAlex";
     //test
     public static void main(String[] args) {
         int a = 1;
