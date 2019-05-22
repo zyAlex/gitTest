d044cfe4839cf12e9704dbb0357769fa41ff2797
zyAlex <37410726+zyAlex@users.noreply.github.com>
2019-05-22 10:11:17 +0800

    Update Hellow.java

--- src/main/Hellow.java
+++ src/main/Hellow.java
@@ -1,7 +1,7 @@
 public class Hellow {
     private String id = "";
     private String name = "";
-
+    //test
     public static void main(String[] args) {
         int a = 1;
         System.out.println("");
